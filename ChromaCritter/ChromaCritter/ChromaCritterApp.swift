170dc6a6397a6e6cc0e2f73e4b0acda4662b1b24
Krisma Antonio <112125209+krisma-antonio@users.noreply.github.com>
2023-09-25 14:44:51 -0700

    Add files via upload

---
+++ ChromaCritter/ChromaCritter/ChromaCritterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChromaCritterApp.swift
+//  ChromaCritter
+//
+//  Created by Krisma Antonio on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ChromaCritterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
