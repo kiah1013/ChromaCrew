4d934b6f112bd97004995a49f3172a94f57d6991
Krisma Antonio <krismapaulene.antonio681@myci.csuci.edu>
2023-10-20 18:14:43 -0700

    pushing changed/added files

--- ChromaCritters/ChromaCritters/Line.swift
+++ ChromaCritters/ChromaCritters/Line.swift
@@ -14,12 +14,52 @@ enum DrawingTool: Codable {
     case eraser
 }
 
-struct Line {
+struct Line: Identifiable, Codable {
     let tool: DrawingTool
     var points: [CGPoint]
-    var color: Color
+    var color: Color {
+        get {
+            customColor.color
+        }
+        set {
+            customColor = CustomColor(color: newValue)
+        }
+    }
+    private var customColor: CustomColor
     var lineWidth: CGFloat
+    let id: UUID
+    
+    init(tool: DrawingTool, points: [CGPoint], color: Color, lineWidth: CGFloat) {
+        self.tool = tool
+        self.points = points
+        self.customColor = CustomColor(color: color)
+        self.lineWidth = lineWidth
+        self.id = UUID()
+    }
+    
     
 }
 
-
+struct CustomColor: Codable {
+    var green: Double = 1
+    var blue: Double = 0
+    var red: Double = 1
+    var opacity: Double = 1
+    
+    init(color: Color) {
+        if let components = color.cgColor?.components {
+            if components.count > 2 {
+                self.red = components[0]
+                self.green = components[1]
+                self.blue = components[2]
+            }
+            if components.count > 3 {
+                self.opacity = components[3]
+            }
+        }
+    }
+    
+    var color: Color {
+        Color(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
+    }
+}
