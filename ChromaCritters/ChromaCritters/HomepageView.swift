d5fecbc2a0ea8cb6bc9b312fe839b0732ab9f7fa
Lexi Lashbrook <alexis.lashbrook033@myci.csuci.edu>
2023-11-15 14:42:37 -0800

    Daily recommended image update- fixed scroll

--- ChromaCritters/ChromaCritters/HomepageView.swift
+++ ChromaCritters/ChromaCritters/HomepageView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct HomepageView: View {
     @State private var searchedAnimal = ""
     @State private var selectedPicture = ""
-    //@State private var selectedPic = ""
+    @State private var dailySelected = ""
     @State private var selectedAnimalFilters: [String] = []
     @Environment(\.colorScheme) var colorScheme
 
@@ -21,6 +21,7 @@ struct HomepageView: View {
     
     var body: some View {
         let columnLayout = Array(repeating: GridItem(), count: 2)
+        
         NavigationStack {
             VStack(spacing: 0) {
                 HStack {
@@ -35,79 +36,72 @@ struct HomepageView: View {
                         Image(systemName: "person.crop.circle")
                             .font(.title)
                             .foregroundColor(Color("titleColor"))
-                            
+                        
                     }
                     .padding(.leading, -130)
                     .padding(.top, -60)
                     SearchBarView(searchedAnimal: $searchedAnimal, selectedFilters: $selectedAnimalFilters)
                 }
                 .background(colorScheme == .light
-                ?    LinearGradient(gradient: Gradient(colors:
-                    [Color(red: 254/255, green: 247/255, blue: 158/255),
-                     Color(red:169/255, green: 255/255, blue: 158/255),
-                     Color(red: 158/255, green: 249/255, blue: 252/255),
-                     Color(red: 159/255, green: 158/255, blue: 254/255),]),
-                    startPoint: .topLeading, endPoint: .bottomTrailing)
-                                 
-                : LinearGradient(gradient: Gradient(colors:
-                    [Color(red: 0, green: 0, blue: 0.2),
-                    Color(red: 0.7, green: 0.25, blue: 0.9),
-                    Color(red: 0.5, green: 0.35, blue: 0.9),
-                    Color(red: 0.07, green: 0.2, blue: 0.3),
-                    Color(red: 0, green: 0, blue: 0.2)]),
-                    startPoint: .topLeading, endPoint: .bottomTrailing)
+                            ?    LinearGradient(gradient: Gradient(colors:
+                                                                    [Color(red: 254/255, green: 247/255, blue: 158/255),
+                                                                     Color(red:169/255, green: 255/255, blue: 158/255),
+                                                                     Color(red: 158/255, green: 249/255, blue: 252/255),
+                                                                     Color(red: 159/255, green: 158/255, blue: 254/255),]),
+                                                startPoint: .topLeading, endPoint: .bottomTrailing)
+                            
+                            : LinearGradient(gradient: Gradient(colors:
+                                                                    [Color(red: 0, green: 0, blue: 0.2),
+                                                                     Color(red: 0.7, green: 0.25, blue: 0.9),
+                                                                     Color(red: 0.5, green: 0.35, blue: 0.9),
+                                                                     Color(red: 0.07, green: 0.2, blue: 0.3),
+                                                                     Color(red: 0, green: 0, blue: 0.2)]),
+                                             startPoint: .topLeading, endPoint: .bottomTrailing)
                 )
                 
                 Divider()
-                //DailyImageView()
                 ScrollView {
-                    //DailyImageView()
-                    Spacer()
-                    Spacer()
-                    //DailyImageView()
-                    
-                      NavigationStack {
-                            Text("Daily recommended image:")
-                                .font(.headline)
-                                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                            
-                         Image("dog1")
-                                .resizable()
-                                .scaledToFit()
-                                .border(Color.black)
-                                .clipped() // Keeps pictures within the border
-                                .padding()
-                                .onTapGesture {
-                                    selectedPicture = "dog1" // Updated here
-                                }
-                            NavigationLink("", destination: ColoringPageView(selectedPicture: $selectedPicture), isActive: Binding(
-                                get: { selectedPicture != "" },
-                                set: { if !$0 { selectedPicture = "" } }
-                            ))
-      
-                            
-                        }
-                        .onTapGesture {
-        
+                    NavigationStack {
+                        Text("Daily recommended image:")
+                            .font(.headline)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        
+                        Image("dog")
+                            .resizable()
+                            .scaledToFit()
+                            .border(Color("borderColor"), width: 2)
+                            .clipped() // Keeps pictures within the border
+                            .cornerRadius(15)
+                            .padding()
+                            .onTapGesture {
+                                dailySelected = "dog1" // Updated here
+                                
+                            }
+                        NavigationLink("", destination: ColoringPageView(selectedPicture: $dailySelected), isActive: Binding(
+                            get: { dailySelected != "" },
+                            set: { if !$0 { dailySelected = "" } }
+                        ))
+                    }
+                    ScrollView {
+                        
+                        Spacer()
+                        Spacer()
+                        FilterButtonsView(selectedAnimalFilters: $selectedAnimalFilters)
+                        LazyVGrid(columns: columnLayout) {
+                            ForEach(filteredPicturesArray, id: \.self) { picture in
+                                if searchedAnimal.isEmpty || picture.lowercased().contains(searchedAnimal.lowercased()) {
+                                    VStack {
+                                        Image(picture)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .border(Color("borderColor"), width: 2)
+                                            .clipped() // Keeps pictures within the border
+                                            .cornerRadius(15)
+                                            .padding()
+                                            .onTapGesture {
+                                                selectedPicture = picture+"1" // Updated here
+                                            }
                                     }
-                    
-                    
-                  
-                    FilterButtonsView(selectedAnimalFilters: $selectedAnimalFilters)
-                    LazyVGrid(columns: columnLayout) {
-                        ForEach(filteredPicturesArray, id: \.self) { picture in
-                            if searchedAnimal.isEmpty || picture.lowercased().contains(searchedAnimal.lowercased()) {
-                                VStack {
-                                    Image(picture)
-                                        .resizable()
-                                        .scaledToFit()
-                                        .border(Color("borderColor"), width: 2)
-                                        .clipped() // Keeps pictures within the border
-                                        .cornerRadius(15)
-                                        .padding()
-                                        .onTapGesture {
-                                            selectedPicture = picture+"1" // Updated here
-                                        }
                                 }
                             }
                             // Switches to ColoringPageView when picture is tapped
@@ -116,11 +110,9 @@ struct HomepageView: View {
                                 set: { if !$0 { selectedPicture = "" } }
                             ))
                         }
-                        
-                    
-                    
-                }
-            }.background(Color("customBackground"))
+                    }
+                }.background(Color("customBackground"))
+            }
         }
     }
     
@@ -147,4 +139,3 @@ struct HomepageView_Previews: PreviewProvider {
         }
     }
 }
-
